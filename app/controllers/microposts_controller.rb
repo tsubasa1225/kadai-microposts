aafad311aab44edd9b842ffe49bcf5de148c1268
tubasa ito <tubasa.ito.1225@gmail.com>
2021-06-21 00:59:20 +0000

    follow, unfollow

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :new]
   
   def index
   end
